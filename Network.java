41ce520a38bb6faff7650ea42bd25f105a148517
Andrew Dimarogonas <adimarogonas@gmail.com>
2016-03-12 18:20:33 -0800

    second commit
    Java file for client backend

--- Network.java
+++ Network.java
@@ -1,20 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Organiser;
+package Network;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-/**
- *
- * @author drslowpokephd
- */
-public interface Network {
-    
-    String file = "";
-    
-    public void save();
-    
-    public void push();
-    
-}
+import javax.net.ssl.HttpsURLConnection;
+public class Network {
+		private final String USER_AGENT = "Macbook";
+	
+	public static void main(String[] args) throws Exception {
+		Network http = new Network();
+		System.out.println("Test 1");
+		http.sendGet();
+		
+	}
+
+	
+
+	private void sendGet() throws Exception {
+		
+		// TODO Auto-generated method stub
+		String url = "http://10.2.217:5000/bored";
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+	int responseCode = con.getResponseCode();
+	System.out.println("\nSending 'GET' request to URL : " + url);
+	System.out.println("Response Code : " + responseCode);
+	BufferedReader in = new BufferedReader(
+	        new InputStreamReader(con.getInputStream()));
+	String inputLine;
+	StringBuffer response = new StringBuffer();
+
+	while ((inputLine = in.readLine()) != null) {
+		response.append(inputLine);
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+	}
+	
+	}
+}		 
+
+
